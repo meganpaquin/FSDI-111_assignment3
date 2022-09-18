054436690b2f141b939b6324969491b64b244fbc
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-09-18 12:32:03 -0400

    complete project

--- app/routes.py
+++ app/routes.py
@@ -1,60 +1,92 @@
 from flask import (
     Flask,
     request,
-    render_template
-)
+    render_template)
 
 app = Flask(__name__)
 
 # We created a module called database by adding the __init__.py file into that folder. You can import it like flask or other modules
 from app.database import data_types
 
+RESPONSE = {
+    "Status": "ok"
+}
+
+@app.get("/")
+def home():
+    response = dict(RESPONSE)
+    response["data_types"] = data_types.scan()
+    scan_data = response.get("data_types")
+    return render_template('index.html', data_types=scan_data)
+
 @app.get("/data_types")
 def index():
-    out = data_types.scan()
-    return out
-
-
-
-@app.get("/data_types/integers")
-def integers():
-    out = data_types.select_by_type(name="Integers")
-    return out
-
-@app.get("/data_types/floats")
-def floats():
-    out = data_types.select_by_type(name="Float")
-    return out
-
-@app.get("/data_types/booleans")
-def booleans():
-    out = data_types.select_by_type(name="Booleans")
-    return out
+    response = dict(RESPONSE)
+    response["data_types"] = data_types.scan()
+    scan_data = response.get("data_types")
+    return render_template('index.html', data_types=scan_data)
 
 @app.get("/data_types/about")
 def about():
     return render_template("about.html")
 
-@app.get("/data_types/strings")
-def strings():
-    return render_template("strings.html")
+@app.get("/data_types/Integers")
+def Integers():
+    response = dict(RESPONSE)
+    response["data_type"] = data_types.select_by_type(name="Integers")
+    scan_data = response.get("data_type")
+    return render_template('data_type.html', data_type=scan_data[0])
 
-@app.get("/data_types/lists")
-def lists():
-    return render_template("lists.html")
+@app.get("/data_types/Floats")
+def Floats():
+    response = dict(RESPONSE)
+    response["data_type"] = data_types.select_by_type(name="Floats")
+    scan_data = response.get("data_type")
+    return render_template('data_type.html', data_type=scan_data[0])
+
+@app.get("/data_types/Booleans")
+def Booleans():
+    response = dict(RESPONSE)
+    response["data_type"] = data_types.select_by_type(name="Booleans")
+    scan_data = response.get("data_type")
+    return render_template('data_type.html', data_type=scan_data[0])
+
+@app.get("/data_types/Strings")
+def Strings():
+    response = dict(RESPONSE)
+    response["data_type"] = data_types.select_by_type(name="Strings")
+    scan_data = response.get("data_type")
+    return render_template('data_type.html', data_type=scan_data[0])
+
+@app.get("/data_types/Lists")
+def Lists():
+    response = dict(RESPONSE)
+    response["data_type"] = data_types.select_by_type(name="Lists")
+    scan_data = response.get("data_type")
+    return render_template('data_type.html', data_type=scan_data[0])
+
+@app.get("/data_types/Tuples")
+def Tuples():
+    response = dict(RESPONSE)
+    response["data_type"] = data_types.select_by_type(name="Tuples")
+    scan_data = response.get("data_type")
+    return render_template('data_type.html', data_type=scan_data[0])
+
+@app.get("/data_types/Dictionaries")
+def Dictionaries():
+    response = dict(RESPONSE)
+    response["data_type"] = data_types.select_by_type(name="Dictionaries")
+    scan_data = response.get("data_type")
+    return render_template('data_type.html', data_type=scan_data[0])
+
 
 @app.get("/data_types/reference")
 def reference():
     return render_template("reference.html")
 
-@app.get("/data_types/tuples")
-def tuples():
-    return render_template("tuples.html)")
 
-@app.get("/data_types/dictionaries")
-def dictionaries():
-    return render_template("dictionaries.html")
 
+# Add, delete, change the database
 @app.post("/data_types")
 def create_data_type():
     data_body = request.json
--- setup.sql
+++ setup.sql
@@ -37,3 +37,47 @@ INSERT INTO data_type (
     "True or false values",
     "A data type that is either true or false"
 );
+
+INSERT INTO data_type (
+    name,
+    summary,
+    description
+) VALUES (
+    "Strings",
+    "Used for letters and words",
+    "A collection of alphabets, words, or characters"
+);
+
+INSERT INTO data_type (
+    name,
+    summary,
+    description
+) VALUES (
+    "Lists",
+    "An ordered sequence of elements",
+    "A changeable and ordered sequence of elements. A way to hold data defined by [] brackets."
+);
+
+INSERT INTO data_type (
+    name,
+    summary,
+    description
+) VALUES (
+    "Dictionaries",
+    "A collection of key-value pairs",
+    "A collection of data defined in key-value pairs. Where keys cannot be repeated but values can be repeated. Defined by {} brackets."
+);
+
+INSERT INTO data_type (
+    name,
+    summary,
+    description
+) VALUES (
+    "Tuples",
+    "Single variables collected together",
+    "A collection of single variables stored in one variable. It is ordered and unchanegable and declared with () brackets."
+);
+
+UPDATE data_type SET description = "A floating point number. Used to represent real numbers and is written with a decimal point to divide fractional parts." WHERE id=2;
+
+UPDATE data_type SET description = "A built in data type that can either be true (1) or false (0)." WHERE id=3;
